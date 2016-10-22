9519cbafb30b74e05a1e02424e2723b8c3b5e3ae
Andrii Petruk <andrey.petruk@gmail.com>
2016-10-22 23:25:32 +0300

    update

--- img.py
+++ img.py
@@ -10,23 +10,26 @@ class Image:
 
    BLOCK_SIZE = 20 # var for set mini image size
 
-   def __init__(self,filename):
+   def __init__(self, filename):
        self.filename = filename
 
    def load(self):
-       img = pImage.open(self.filename) #load file
-       mini_img = img.resize((Image.BLOCK_SIZE, Image.BLOCK_SIZE), pImage.BILINEAR) #resize image
-       self.t_data = numpy.array(
-           [sum(list(x)) for x in mini_img.getdata()]      # create array base on image data
-       )
-       del mini_img, img    # clear vars
+       try:
+           img = pImage.open(self.filename) #load file
+           mini_img = img.resize((Image.BLOCK_SIZE, Image.BLOCK_SIZE), pImage.BILINEAR) #resize image
+           self.t_data = numpy.array(
+              [sum(list(x)) for x in mini_img.getdata()]      # create array base on image data
+           )
+           del mini_img, img    # clear vars
+       except:
+             print('\n Load error:  %s' % self.filename)
        return self
 
    def __repr__(self):
         return self.filename      # return filename
 
 
-class ImagesList:
+class ImageList:
     """Build images list for directory"""
 
     def __init__(self, dirname):
@@ -34,12 +37,29 @@ class ImagesList:
         self.load()
 
     def load(self):
-        self.images = Image([os.path.join(self.dirname, filename) for filename in os.listdir(self.dirname)
-                             if filename.endswith('*.jpg')]).load()
+        self.images = \
+            [Image(os.path.join(self.dirname, filename)).load() \
+                       for filename in os.listdir(self.dirname)
+                           if filename.endswith('.jpg')]
         return self
 
     def __repr__(self):
-        return self.images
+        return '\n'.join((x.filename for x in self.images))
 
+    def html(self):
+        body = ['<html><body>']
+        body += ['<img src="'+x.filename+'" width="200"/>' for x in self.images]
+        body += ['</body</html>']
+        return '\n'.join(body)
+
+
+
+if __name__ == '__main__':
+    my = ImageList('/home/pag/fs/photos/')
+    try:
+        indexhtml = open('/home/pag/fs/index.html', 'w')
+        indexhtml.write(my.html())
+    except:
+        print("error build index.html")
 
 
